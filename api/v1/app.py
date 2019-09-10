a00b4a56858c59ca330af52e8d263ad53b6fe0f9
klupzor <lopezgand@gmail.com>
2019-09-10 21:49:44 +0000

    Making not found page

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,22 +3,23 @@
 starts a Flask web application
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
- 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
 
 
 @app.teardown_appcontext
 def close(cls):
-	storage.close()
+    storage.close()
+
+
+@app.errorhandler(404)
+def error(e):
+    return jsonify({"error": "Not found"})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', threaded=True)
