9cf6e3edf5bee310ab9b0f8b5ba4d993d93cb420
heimerr <732@holbertonschool.com>
2019-09-11 19:30:04 +0000

    [fix] import Amenity fixed

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.city import Amenity
+from models.amenity import Amenity
 
 
 @app_views.route('/amenities')
