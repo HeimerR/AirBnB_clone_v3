6c26c8277c798a00e642bd9943b1a9c50cad0237
klupzor <lopezgand@gmail.com>
2019-09-11 00:26:55 +0000

    demo delete state

---
+++ api/v1/views/states.py
@@ -0,0 +1,30 @@
+from api.v1.views import app_views
+from flask import jsonify, abort
+from models import storage
+
+
+@app_views.route('/states/<id>')
+@app_views.route('/states')
+def state(id=None):
+    states=[]
+    if id:
+        state = storage.get("State", id)
+        if state is None:
+            abort(404, description="Resource not found")
+        else:
+            return jsonify(state.to_dict())
+
+    for state in storage.all("State").values():
+        states.append(state.to_dict())
+    return jsonify(states)
+
+
+
+@app_views.route('/states/<id>', methods=['DELETE'])
+def state_delete(id=None):
+    state = storage.get("State", id)
+    if state is None:
+        abort(404, description="Resource not found")
+    else:
+        state.delete()
+        return jsonify({}, 200)
