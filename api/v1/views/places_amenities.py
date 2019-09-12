000fe94030d56f1cd429bbef8f831ba59da75d6f
heimerr <732@holbertonschool.com>
2019-09-12 13:23:11 +0000

    [feature] place amenities GET method with filestorage added

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,8 +5,10 @@ from flask import jsonify, abort, request
 from models import storage
 from models.review import Review
 from models.amenity import Amenity
+import os
 
 
+db = os.environ.get('HBNB_TYPE_STORAGE', None)
 @app_views.route('/places/<place_id>/amenities')
 def amenities_from_place(place_id=None):
     """ GET amenities from place """
@@ -15,20 +17,24 @@ def amenities_from_place(place_id=None):
     if place is None:
         abort(404)
     else:
-        for amenity in place.amenities:
-            amenities.append(amenity.to_dict())
-        return jsonify(amenities)
+        if db is 'db':
+            for amenity in place.amenities:
+                amenities.append(amenity.to_dict())
+            return jsonify(amenities)
+        else:
+            jsonnify(place.amenity_ids)
+
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'])
 def amenity_review_delete(place_id=None, amenity_id=None):
-    """ DELETE amenities  methods """
+    """ DELETE amenities from a place methods """
     place = storage.get("Place", place_id)
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None or place is None:
         abort(404)
     if not amenity.id  in place.amenities.id:
         abort(404)
-    amenity.delete()
+
     storage.save()
     return (jsonify({}), 200)
 
