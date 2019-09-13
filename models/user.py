32b39ce7005594715d7175631986c45c7f63006b
heimerr <732@holbertonschool.com>
2019-09-13 03:06:35 +0000

    [fix] test user.py with password

--- models/user.py
+++ models/user.py
@@ -14,7 +14,6 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship(
@@ -30,8 +29,10 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+    password = Column(String(128), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        passwmd5 = hashlib.md5(kwargs["password"].encode())
+        passwmd5 = hashlib.md5(str(self.password).encode())
         self.password = passwmd5.hexdigest()
