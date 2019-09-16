fe1778f8071c1c25f1954aa589957d4252a58eda
klupzor <lopezgand@gmail.com>
2019-09-16 16:05:04 +0000

    places amenities documentation

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -14,7 +14,21 @@ db = os.environ.get('HBNB_TYPE_STORAGE', 'jsonfile')
 
 @app_views.route('/places/<place_id>/amenities')
 def amenities_from_place(place_id=None):
-    """ GET amenities from place """
+    """ Show amenities from place
+    ---
+    tags:
+        - Place Amenities
+    parameters:
+      - name: place_id
+        in: path
+        type: string
+        required: true
+    responses:
+      200:
+        description: Show amenity
+      404:
+        description: Resource not found
+    """
     amenities = []
     place = storage.get("Place", place_id)
     if place is None:
@@ -32,7 +46,25 @@ def amenities_from_place(place_id=None):
     '/places/<place_id>/amenities/<amenity_id>',
     methods=['DELETE'])
 def amenity_review_delete(place_id=None, amenity_id=None):
-    """ DELETE amenities from a place methods """
+    """ Delete amenities from a place
+    ---
+    tags:
+        - Place Amenities
+    parameters:
+      - name: place_id
+        in: path
+        type: string
+        required: true
+      - name: amenity_id
+        in: path
+        type: string
+        required: true
+    responses:
+      200:
+        description: Amenity delated
+      404:
+        description: Resource not found
+    """
     place = storage.get("Place", place_id)
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None or place is None:
@@ -51,7 +83,25 @@ def amenity_review_delete(place_id=None, amenity_id=None):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
 def amenity_from_place_post(place_id, amenity_id):
-    """ POST method to create a amenity """
+    """ Create a amenity
+    ---
+    tags:
+        - Place Amenities
+    parameters:
+      - name: place_id
+        in: path
+        type: string
+        required: true
+      - name: amenity_id
+        in: path
+        type: string
+        required: true
+    responses:
+      201:
+        description: Amenity created!
+      404:
+        description: Resource not found
+    """
     place = storage.get("Place", place_id)
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None or place is None:
